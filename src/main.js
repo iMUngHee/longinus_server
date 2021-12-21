2b7ce9a062526be32f53ea67e5903b60569d4a48
Hee <unghee66@gmail.com>
2021-12-21 22:36:30 +0900

    Add Admin Control & Connet admint to post

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import jwtMiddleware from './lib/jwtMiddleware';
 import Router from 'koa-router';
 import mongoose from 'mongoose';
 
@@ -25,6 +26,7 @@ router.use('/api', api.routes()); // api 라우트 적용
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
