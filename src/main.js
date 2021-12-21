d8f9740c67310a02a533e14e1ce77ac8c6ab1bb1
Hee <unghee66@gmail.com>
2021-12-21 21:14:32 +0900

    Add Post Control

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 import Router from 'koa-router';
 import mongoose from 'mongoose';
+
+import api from './api';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -17,10 +20,13 @@ mongoose
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx) => {
-	ctx.body = 'hello World!';
-});
+// 라우터 설정
+router.use('/api', api.routes()); // api 라우트 적용
 
+// 라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
